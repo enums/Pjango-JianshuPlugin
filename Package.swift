944473087aad63ab744e3ef00a0ca482a0bca0a9
enums <enum@enumsblog.com>
2017-07-01 19:03:08 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ let package = Package(
     name: "Pjango-JianshuPlugin",
     targets: [],
     dependencies: [
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", majorVersion: 2),
         .Package(url: "https://github.com/enums/Pjango.git", majorVersion: 0),
         .Package(url: "https://github.com/enums/SwiftyJSON.git", majorVersion: 4),
     ]
