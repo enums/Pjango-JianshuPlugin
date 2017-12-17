6d472a67c7999e3bcf92218772f90d70bbfa20f5
zhengyuqi <zhengyuqi@baidu.com>
2017-12-18 00:05:55 +0800

    update to Perfect 3.x

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "Pjango-JianshuPlugin",
     targets: [],
     dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", majorVersion: 2),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", majorVersion: 3),
         .Package(url: "https://github.com/enums/Pjango.git", majorVersion: 0, minor: 9),
         .Package(url: "https://github.com/enums/SwiftyJSON.git", majorVersion: 4),
     ]
